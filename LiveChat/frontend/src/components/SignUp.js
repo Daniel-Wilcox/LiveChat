69bdb8bda388d700b858889008f4568150502151
Daniel <d.dt.wilcox@gmail.com>
2021-03-30 00:48:40 +0200

    Forms mostly working

--- LiveChat/frontend/src/components/SignUp.js
+++ LiveChat/frontend/src/components/SignUp.js
@@ -5,164 +5,154 @@ import {Link} from 'react-router-dom'
 
 function SignUp() {
 
-    const [username, setUsername] = useState('');
-    const [pass1, setPass1] = useState('');
-    const [pass2, setPass2] = useState('');
-    const [date_of_birth, setDateOfBirth] = useState('');
-    const [email, setEmail] = useState('');
-    const [check, setCheck] = useState(false);
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + "-" + mm + '-' + dd;
+    
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const [username, setUsername] = useState("");
+    const [pass1, setPass1] = useState("");
+    const [pass2, setPass2] = useState("");
+    const [date_of_birth, setDateOfBirth] = useState(today);
+    const [email, setEmail] = useState("");
 
-    // const handleUpdate = (e, updater) => {
-    //     updater(e.target.value);
-    //     console.log(username);
+    const [check1, setCheck] = useState(false);
 
-    // }
+
 
     const handleSubmit = (e) => {
-        console.log(`username {username}`);
-        // console.log(username)
-        // console.log(username)
-        // console.log(username)
-        // console.log(username)
+
+
+        console.log(username)
+        console.log(pass1)
+        console.log(pass2)
+        console.log(date_of_birth)
+        console.log(email)
+
+
+        
+        // e.preventDefault()
+        // const formData = new FormData(e.target),
+        //     formDataObj = Object.fromEntries(formData.entries())
+        // console.log(formDataObj)
 
     };
 
 
-
-
     return (
         <>
-          <Button variant="primary" onClick={handleShow}>
-            Sign Up
-          </Button>
-    
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Sign Up</Modal.Title>
-            </Modal.Header>
+            <Button variant="primary" onClick={handleShow}> Sign Up </Button>
+            <Modal show={show} onHide={handleClose}>
 
-            <Modal.Body>
-                <Form>
-                <Form.Group controlId="formUsername">
-                    <Form.Label>User Name</Form.Label>
-                    <Form.Control required type="username" placeholder="Enter username" onChange={() => setUsername(e.target.value)} />
-                    <Form.Text className="text-muted">
-                        This is the username that others will see in LiveChat.
-                    </Form.Text>
-                </Form.Group>
-{/* 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control required type="password" placeholder="Password" onChange={() => setPass1(e.target.value)}/>
-                </Form.Group>
+                <Modal.Header closeButton>
+                    <Modal.Title> Sign Up </Modal.Title>
+                </Modal.Header>
 
-                <Form.Group controlId="formBasicPasswordConfirm">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control required type="password" placeholder="Confirm Password" onChange={() => setPass2(e.target.value)}/>
-                </Form.Group>
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit}>
 
-                <Form.Group controlId="formDOB">
-                    <Form.Label>Select Date of Birth</Form.Label>
-                    <Form.Control required type="date" name="dob" placeholder="Date of Birth" onChange={() => setDateOfBirth(e.target.value)}/>
-                </Form.Group>
+                        {/* Username Form Input */}
+                        <Form.Group controlId="formUsername">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Enter username"
+                                value={username}
+                                onChange={(e) => {setUsername(e.target.value)}}
+                            />
+                            <Form.Text className="text-muted">
+                                This is the username that others will see in LiveChat.
+                            </Form.Text>
+                        </Form.Group>
 
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control required type="email" placeholder="Enter email" onChange={() => setEmail(e.target.value)}/>
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                        {/* Password Form Input */}
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="Enter Password"
+                                value={pass1}
+                                onChange={(e) => {setPass1(e.target.value)}}
+                            />
+                        </Form.Group>
 
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check required type="checkbox" label="Confirm above information" onChange={() => setCheck(e.target.value)}/>
-                    <Form.Text className="text-muted">
-                        By clicking Sign Up, you are indicating that you have read and acknowledge the <Link to='p/tos'>Terms of Service</Link> and <Link to='p/Privacy'>Privacy Notice</Link>.
-                    </Form.Text>
-                </Form.Group> */}
+                        {/* Password 2 Form Input */}
+                        <Form.Group controlId="formConfirmPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="Confirm Password"
+                                value={pass2}
+                                onChange={(e) => {setPass2(e.target.value)}}
+                            />
+                        </Form.Group>
 
-   
-                
+                         {/* DOB Form Input */}
+                        <Form.Group controlId="formDateOfBirth">
+                            <Form.Label>Date of Birth</Form.Label>
+                            <Form.Control
+                                required
+                                type="date"
+                                placeholder="Select Date of Birth"
+                                value={date_of_birth}
+                                onChange={(e) => {setDateOfBirth(e.target.value)}}
+                            />
+                        </Form.Group>
 
-                </Form>
-            </Modal.Body>
-            
-            <Modal.Footer>
-            <Button variant="primary" type="submit" onClick={(handleSubmit)}>Submit</Button>
-            {/* <Button variant="primary" type="submit" onClick={(handleSubmit)}>Submit</Button> */}
-            </Modal.Footer>
-          </Modal>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                required
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e) => {setEmail(e.target.value)}}
+                            />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+
+                        <Form.Group controlId="formTOS">
+                            <Form.Check 
+                                required
+                                type="checkbox"
+                                label="Confirm above information"
+                                feedback="You must agree before completing the sign up form."
+                                value={check1}
+                                onChange={(e) => {setCheck(e.target.value)}}
+                            />
+                            <Form.Text className="text-muted">
+                                By clicking Sign Up, you are indicating that you have read and acknowledge the <Link to='p/tos'>Terms of Service</Link> and <Link to='p/Privacy'>Privacy Notice</Link>.
+                            </Form.Text>
+
+                        </Form.Group>
+
+
+                       
+                        <Button variant="primary" type="submit"> 
+                            Create Account 
+                        </Button> 
+
+                    </Form>
+                </Modal.Body>
+
+            </Modal>
+
         </>
-      );
-    // return (
-    //     <>
-    //       <Button variant="primary" onClick={handleShow}>
-    //         Sign Up
-    //       </Button>
-    
-    //       <Modal show={show} onHide={handleClose}>
-    //         <Modal.Header closeButton>
-    //           <Modal.Title>Sign Up</Modal.Title>
-    //         </Modal.Header>
+        
+    );
 
-    //         <Modal.Body>
-    //             <Form>
-    //             <Form.Group controlId="formUsername">
-    //                 <Form.Label>User Name</Form.Label>
-    //                 <Form.Control required type="username" placeholder="Enter username" onChange={() => setUsername(e.target.value)} />
-    //                 <Form.Text className="text-muted">
-    //                     This is the username that others will see in LiveChat.
-    //                 </Form.Text>
-    //             </Form.Group>
-
-    //             <Form.Group controlId="formBasicPassword">
-    //                 <Form.Label>Password</Form.Label>
-    //                 <Form.Control required type="password" placeholder="Password" onChange={() => setPass1(e.target.value)}/>
-    //             </Form.Group>
-
-    //             <Form.Group controlId="formBasicPasswordConfirm">
-    //                 <Form.Label>Password</Form.Label>
-    //                 <Form.Control required type="password" placeholder="Confirm Password" onChange={() => setPass2(e.target.value)}/>
-    //             </Form.Group>
-
-    //             <Form.Group controlId="formDOB">
-    //                 <Form.Label>Select Date of Birth</Form.Label>
-    //                 <Form.Control required type="date" name="dob" placeholder="Date of Birth" onChange={() => setDateOfBirth(e.target.value)}/>
-    //             </Form.Group>
-
-    //             <Form.Group controlId="formBasicEmail">
-    //                 <Form.Label>Email address</Form.Label>
-    //                 <Form.Control required type="email" placeholder="Enter email" onChange={() => setEmail(e.target.value)}/>
-    //                 <Form.Text className="text-muted">
-    //                     We'll never share your email with anyone else.
-    //                 </Form.Text>
-    //             </Form.Group>
-
-    //             <Form.Group controlId="formBasicCheckbox">
-    //                 <Form.Check required type="checkbox" label="Confirm above information" onChange={() => setCheck(e.target.value)}/>
-    //                 <Form.Text className="text-muted">
-    //                     By clicking Sign Up, you are indicating that you have read and acknowledge the <Link to='p/tos'>Terms of Service</Link> and <Link to='p/Privacy'>Privacy Notice</Link>.
-    //                 </Form.Text>
-    //             </Form.Group>
-
-   
-                
-
-    //             </Form>
-    //         </Modal.Body>
-            
-    //         <Modal.Footer>
-    //         <Button variant="primary" type="submit" onClick={(handleSubmit)}>Submit</Button>
-    //         {/* <Button variant="primary" type="submit" onClick={(handleSubmit)}>Submit</Button> */}
-    //         </Modal.Footer>
-    //       </Modal>
-    //     </>
-    //   );
 }
 
 export default SignUp
